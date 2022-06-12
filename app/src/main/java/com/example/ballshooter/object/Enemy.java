6bee08d8ea1600ebf0aa1e920d1f20f2717a1c61
IT20281700 <it20281700@my.sliit.lk>
2022-06-13 01:21:22 +0530

    Collision detection

--- app/src/main/java/com/example/ballshooter/object/Enemy.java
+++ app/src/main/java/com/example/ballshooter/object/Enemy.java
@@ -12,14 +12,44 @@ import com.example.ballshooter.R;
  * The Enemy class is an extension of a Circle, which is an extension of a GameObject
  */
 public class Enemy extends Circle {
-    private final Player player;
     private static final double SPEED_PIXELS_PER_SECOND = Player.SPEED_PIXELS_PER_SECOND*0.6;
     private static final double MAX_SPEED = SPEED_PIXELS_PER_SECOND / GameLoop.MAX_UPS;
+    private static final double SPAWNS_PER_MINUTE = 20;
+    private static final double SPAWNS_PER_SECOND = SPAWNS_PER_MINUTE/60.0;
+    private static final double UPDATES_PER_SPAWN = GameLoop.MAX_UPS/SPAWNS_PER_SECOND;
+    private static double updatesUntilNextSpawn = UPDATES_PER_SPAWN;
+    private final Player player;
 
     public Enemy(Context context, Player player, double positionX, double positionY, double radius) {
         super(context, ContextCompat.getColor(context, R.color.enemy), positionX, positionY, radius);
         this.player = player;
     }
+
+    public Enemy(Context context, Player player) {
+        super(
+                context,
+                ContextCompat.getColor(context,R.color.enemy),
+                Math.random()*1000,
+                Math.random()*1000,
+                30
+        );
+        this.player = player;
+    }
+
+    /**
+     * readyToSpawn checks if a new enemy should spawn, according to the decided number spawns
+     * per minute (See SPAWNS_PER_MINUTE at top)
+     * @return
+     */
+    public static boolean readyToSpawn() {
+        if(updatesUntilNextSpawn <= 0) {
+            updatesUntilNextSpawn += UPDATES_PER_SPAWN;
+            return true;
+        } else {
+            updatesUntilNextSpawn --;
+            return false;
+        }
+     }
 
     @Override
     public void update() {
